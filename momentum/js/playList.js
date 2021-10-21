9b17f38a7945c157997de48ef8ca4486b7fb35d4
Antonina Berchuk <tonyaberch@gmail.com>
2021-10-21 18:46:16 +0200

    fix: change url audio

--- momentum/js/playList.js
+++ momentum/js/playList.js
@@ -1,17 +1,17 @@
 const playList = [
   {
     title: 'Aqua Caelestis',
-    src: '../assets/sounds/Aqua Caelestis.mp3',
+    src: './assets/sounds/Aqua Caelestis.mp3',
     duration: '00:39'
   },
   {
     title: 'River Flows In You',
-    src: '../assets/sounds/River Flows In You.mp3',
+    src: './assets/sounds/River Flows In You.mp3',
     duration: '01:37'
   },
   {
     title: 'Ennio Morricone',
-    src: '../assets/sounds/Ennio Morricone.mp3',
+    src: './assets/sounds/Ennio Morricone.mp3',
     duration: '01:37'
   },
   {
